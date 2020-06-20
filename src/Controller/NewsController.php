9501570f26abe5969bfd65dd7189331cb0619095
ndibrahima <ndiayeibrahim666@gmail.com>
2020-06-20 19:00:53 +0200

    commit aprés ajout de CORS

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity;
+use App\Entity\Society;
+use App\Entity\News;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class NewsController extends AbstractController
+{
+    /**
+     * @Route("/news_list", methods={"GET"}, name="news_list")
+     * 
+     */ 
+    public function index() {
+
+        $news= $this->getDoctrine()->getRepository
+        (News::class)->findAll();
+
+        return $this->render('news/index.html.twig', array ('news' => $news));
+     }
+    
+    /**
+     * @Route("/news/add", name="news_add")
+     */
+    public function addNews(Request $request) {
+        $news = new News();
+        $form = $this->createFormBuilder($news)
+            ->add('name', TextType::class)
+            ->add('date', DateType::class)
+            ->add('description', TextAreaType::class)
+            ->add('society', EntityType::class, array(
+                'class'=>'App\Entity\Society',
+                'choice_label'=>'name',
+                'expanded'=>false,
+                'multiple'=>false
+            ))
+            ->add('save', SubmitType::class, ['label' => 'Ajouter une actualité'])
+            ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $news = $form->getData();
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($news);
+                $entityManager->flush();
+                return $this->redirectToRoute('news_list');
+            }
+            return $this->render('news/new.html.twig', [
+                'form' => $form->createView(),
+            ]);
+    } 
+    
+
+
+     /**
+     * @Route("/news/update/{id}", methods={"GET", "POST"}, name="news_update")
+     */
+    public function updateNews(Request $request, $id) {
+
+        $news = new News();
+        $news = $this->getDoctrine()->getRepository
+       (News::class)->find($id);
+
+        $form = $this->createFormBuilder($news)
+        ->add('name', TextType::class)
+        ->add('date', DateType::class)
+        ->add('description', TextType::class)
+        ->add('society', EntityType::class, array(
+            'class'=>'App\Entity\Society',
+            'choice_label'=>'type',
+            'expanded'=>false,
+            'multiple'=>false
+        ))
+        ->add('save', SubmitType::class, ['label' => 'Modifier'])
+        ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->flush();
+        
+                return $this->redirectToRoute('news_list');
+            }
+
+            return $this->render('news/update.html.twig', [
+                'form' => $form->createView(),
+            ]);
+    }
+
+
+   /**
+     * @Route("/news/{id}", name="news_show")
+     */ 
+    public function showNews($id) {
+        $news = $this->getDoctrine()->getRepository
+        (News::class)->find($id);
+ 
+        return $this->render('news/show.html.twig', array 
+        ('news' => $news));
+     }
+    
+
+    /**
+     * @Route("/news/delete/{id}", methods={"DELETE"}, name="news_delete")
+     * 
+     */ 
+
+    public function deleteNews(Request $request, $id){
+    $news = $this->getDoctrine()-> getRepository(News::class)->find($id);
+    
+    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager->remove($news);
+    $entityManager->flush();
+
+    $response = new Response();
+    $response->send();
+
+    }
+}
--- src/Migrations/Version20200615234103.php
+++ src/Migrations/Version20200615234103.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200525202754 extends AbstractMigration
+final class Version20200615234103 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -30,14 +30,19 @@ final class Version20200525202754 extends AbstractMigration
         $this->addSql('CREATE TABLE advantage_society (advantage_id INT NOT NULL, society_id INT NOT NULL, INDEX IDX_9E6A6D683864498A (advantage_id), INDEX IDX_9E6A6D68E6389D24 (society_id), PRIMARY KEY(advantage_id, society_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE card (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE card_society (card_id INT NOT NULL, society_id INT NOT NULL, INDEX IDX_5A7EEB844ACC9A20 (card_id), INDEX IDX_5A7EEB84E6389D24 (society_id), PRIMARY KEY(card_id, society_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, message VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE good_plan (id INT AUTO_INCREMENT NOT NULL, society_id INT NOT NULL, address_id INT NOT NULL, name VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7F06E7EFE6389D24 (society_id), UNIQUE INDEX UNIQ_7F06E7EFF5B7AF75 (address_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE help (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE help_requirement (help_id INT NOT NULL, requirement_id INT NOT NULL, INDEX IDX_35DBE1B4D3F165E7 (help_id), INDEX IDX_35DBE1B47B576F77 (requirement_id), PRIMARY KEY(help_id, requirement_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE help_tag (help_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_56E059AED3F165E7 (help_id), INDEX IDX_56E059AEBAD26311 (tag_id), PRIMARY KEY(help_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE help_society (help_id INT NOT NULL, society_id INT NOT NULL, INDEX IDX_537E9E2ED3F165E7 (help_id), INDEX IDX_537E9E2EE6389D24 (society_id), PRIMARY KEY(help_id, society_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE news (id INT AUTO_INCREMENT NOT NULL, society_id INT NOT NULL, name VARCHAR(255) NOT NULL, date DATE NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1DD39950E6389D24 (society_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE place (id INT AUTO_INCREMENT NOT NULL, society_id INT NOT NULL, name VARCHAR(255) NOT NULL, date DATE NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_741D53CDE6389D24 (society_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE requirement (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, operator VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, value VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE society (id INT AUTO_INCREMENT NOT NULL, address_id INT NOT NULL, phone INT NOT NULL, name VARCHAR(100) NOT NULL, type VARCHAR(100) NOT NULL, website VARCHAR(150) NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_D6461F2F5B7AF75 (address_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(100) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(100) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE actuality_society ADD CONSTRAINT FK_A5C1B307B84BD854 FOREIGN KEY (actuality_id) REFERENCES actuality (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE actuality_society ADD CONSTRAINT FK_A5C1B307E6389D24 FOREIGN KEY (society_id) REFERENCES society (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE advantage_tag ADD CONSTRAINT FK_6508D1673864498A FOREIGN KEY (advantage_id) REFERENCES advantage (id) ON DELETE CASCADE');
@@ -46,12 +51,16 @@ final class Version20200525202754 extends AbstractMigration
         $this->addSql('ALTER TABLE advantage_society ADD CONSTRAINT FK_9E6A6D68E6389D24 FOREIGN KEY (society_id) REFERENCES society (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE card_society ADD CONSTRAINT FK_5A7EEB844ACC9A20 FOREIGN KEY (card_id) REFERENCES card (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE card_society ADD CONSTRAINT FK_5A7EEB84E6389D24 FOREIGN KEY (society_id) REFERENCES society (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE good_plan ADD CONSTRAINT FK_7F06E7EFE6389D24 FOREIGN KEY (society_id) REFERENCES society (id)');
+        $this->addSql('ALTER TABLE good_plan ADD CONSTRAINT FK_7F06E7EFF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
         $this->addSql('ALTER TABLE help_requirement ADD CONSTRAINT FK_35DBE1B4D3F165E7 FOREIGN KEY (help_id) REFERENCES help (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE help_requirement ADD CONSTRAINT FK_35DBE1B47B576F77 FOREIGN KEY (requirement_id) REFERENCES requirement (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE help_tag ADD CONSTRAINT FK_56E059AED3F165E7 FOREIGN KEY (help_id) REFERENCES help (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE help_tag ADD CONSTRAINT FK_56E059AEBAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE help_society ADD CONSTRAINT FK_537E9E2ED3F165E7 FOREIGN KEY (help_id) REFERENCES help (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE help_society ADD CONSTRAINT FK_537E9E2EE6389D24 FOREIGN KEY (society_id) REFERENCES society (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE news ADD CONSTRAINT FK_1DD39950E6389D24 FOREIGN KEY (society_id) REFERENCES society (id)');
+        $this->addSql('ALTER TABLE place ADD CONSTRAINT FK_741D53CDE6389D24 FOREIGN KEY (society_id) REFERENCES society (id)');
         $this->addSql('ALTER TABLE society ADD CONSTRAINT FK_D6461F2F5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
     }
 
@@ -61,6 +70,7 @@ final class Version20200525202754 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE actuality_society DROP FOREIGN KEY FK_A5C1B307B84BD854');
+        $this->addSql('ALTER TABLE good_plan DROP FOREIGN KEY FK_7F06E7EFF5B7AF75');
         $this->addSql('ALTER TABLE society DROP FOREIGN KEY FK_D6461F2F5B7AF75');
         $this->addSql('ALTER TABLE advantage_tag DROP FOREIGN KEY FK_6508D1673864498A');
         $this->addSql('ALTER TABLE advantage_society DROP FOREIGN KEY FK_9E6A6D683864498A');
@@ -72,7 +82,10 @@ final class Version20200525202754 extends AbstractMigration
         $this->addSql('ALTER TABLE actuality_society DROP FOREIGN KEY FK_A5C1B307E6389D24');
         $this->addSql('ALTER TABLE advantage_society DROP FOREIGN KEY FK_9E6A6D68E6389D24');
         $this->addSql('ALTER TABLE card_society DROP FOREIGN KEY FK_5A7EEB84E6389D24');
+        $this->addSql('ALTER TABLE good_plan DROP FOREIGN KEY FK_7F06E7EFE6389D24');
         $this->addSql('ALTER TABLE help_society DROP FOREIGN KEY FK_537E9E2EE6389D24');
+        $this->addSql('ALTER TABLE news DROP FOREIGN KEY FK_1DD39950E6389D24');
+        $this->addSql('ALTER TABLE place DROP FOREIGN KEY FK_741D53CDE6389D24');
         $this->addSql('ALTER TABLE advantage_tag DROP FOREIGN KEY FK_6508D167BAD26311');
         $this->addSql('ALTER TABLE help_tag DROP FOREIGN KEY FK_56E059AEBAD26311');
         $this->addSql('DROP TABLE actuality');
@@ -83,10 +96,15 @@ final class Version20200525202754 extends AbstractMigration
         $this->addSql('DROP TABLE advantage_society');
         $this->addSql('DROP TABLE card');
         $this->addSql('DROP TABLE card_society');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE good_plan');
         $this->addSql('DROP TABLE help');
         $this->addSql('DROP TABLE help_requirement');
         $this->addSql('DROP TABLE help_tag');
         $this->addSql('DROP TABLE help_society');
+        $this->addSql('DROP TABLE news');
+        $this->addSql('DROP TABLE place');
         $this->addSql('DROP TABLE requirement');
         $this->addSql('DROP TABLE society');
         $this->addSql('DROP TABLE tag');
